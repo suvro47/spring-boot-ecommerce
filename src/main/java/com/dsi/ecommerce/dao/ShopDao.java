e1394687fd16875f96937af01d2749584d36bf4f
suvro47 <suvro047@gmail.com>
2021-11-10 16:28:19 +0600

    dto-template-introduced

--- src/main/java/com/dsi/ecommerce/dao/ShopDao.java
+++ src/main/java/com/dsi/ecommerce/dao/ShopDao.java
@@ -1,10 +1,10 @@
 package com.dsi.ecommerce.dao;
 
-import com.dsi.ecommerce.model.cart.CartItem;
+import com.dsi.ecommerce.model.Shop;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CartItemDao extends JpaRepository<CartItem, Long> {
+public interface ShopDao extends JpaRepository<Shop, Long> {
 
 }
