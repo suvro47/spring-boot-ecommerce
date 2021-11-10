f6027c7a71c1003a70707661d6ff85cfa7d6ede5
Rashid <rashidul.hasan@dsinnovators.com>
2021-11-10 17:30:42 +0600

    update UserService interface

--- src/main/java/com/dsi/ecommerce/service/UserService.java
+++ src/main/java/com/dsi/ecommerce/service/UserService.java
@@ -1,8 +1,25 @@
 package com.dsi.ecommerce.service;
 
 
+import com.dsi.ecommerce.exception.NoUserFound;
+import com.dsi.ecommerce.exception.UserAlreadyExists;
+import com.dsi.ecommerce.model.User;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface UserService {
+    public User createNewUser(User user) throws UserAlreadyExists;
+
+    public List<User> getAllUsers() throws NoUserFound;
+
+    public void deleteUser(Long userId);
+
+    public User getUserById(Long userId);
+
+    public User getUserFromMyUserDetail(MyUserDetail userDetail );
+
+    public User updateUser(Long userId, User user);
+
 }
