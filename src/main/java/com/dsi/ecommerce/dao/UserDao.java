e215f414c7e484fcc85325a5089d2f31ef420829
jspw <mhshifat757@gmail.com>
2021-11-10 16:58:27 +0600

    user auth service added

--- src/main/java/com/dsi/ecommerce/dao/UserDao.java
+++ src/main/java/com/dsi/ecommerce/dao/UserDao.java
@@ -2,10 +2,12 @@ package com.dsi.ecommerce.dao;
 
 import com.dsi.ecommerce.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserDao extends JpaRepository<User, Long> {
 
+    @Query("select u from user u where u.username = ?1")
     User findByUsername(String username);
 }
--- src/main/java/com/dsi/ecommerce/service/impl/UserServiceImpl.java
+++ src/main/java/com/dsi/ecommerce/service/impl/UserServiceImpl.java
@@ -1,14 +1,9 @@
 package com.dsi.ecommerce.service.impl;
 
-import com.dsi.ecommerce.dao.UserDao;
 import com.dsi.ecommerce.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
 
 @Component
 public class UserServiceImpl implements UserService {
-
-    @Autowired
-    private UserDao userDao ;
-
 }
