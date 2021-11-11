fa0c49abd344847b76bff7b87ec49851e6b4c232
Rashid <rashidul.hasan@dsinnovators.com>
2021-11-11 11:00:54 +0600

    Update UserNotFound exception

--- src/main/java/com/dsi/ecommerce/exception/UserNotFound.java
+++ src/main/java/com/dsi/ecommerce/exception/UserNotFound.java
@@ -3,7 +3,10 @@ package com.dsi.ecommerce.exception;
 import com.dsi.ecommerce.model.User;
 
 public class UserNotFound extends RuntimeException{
+    public UserNotFound(){
+        super(("No User found."));
+    }
     public UserNotFound(long userId) {
-        super("User not found "+ userId);
+        super("User not found with id: "+ userId);
     }
 }
