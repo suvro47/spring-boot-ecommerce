1349d6f9aec555f2ea5ab1e98b00dd9c18561491
Rashid <rashidul.hasan@dsinnovators.com>
2021-11-11 16:08:22 +0600

    Implement Login page

--- src/main/java/com/dsi/ecommerce/service/impl/MyUserDetailService.java
+++ src/main/java/com/dsi/ecommerce/service/impl/MyUserDetailService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class MyUserDetailService implements UserDetailsService {
 
+    @Autowired
     private UserDao userDao;
 
     @Override
