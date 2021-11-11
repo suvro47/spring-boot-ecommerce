df195fe471c70577f0917fb257d2354dd2379df4
NavidMahbub <navidmahbub27@gmail.com>
2021-11-11 12:24:29 +0600

    template updated

--- src/main/java/com/dsi/ecommerce/controller/CartController.java
+++ src/main/java/com/dsi/ecommerce/controller/CartController.java
@@ -2,18 +2,12 @@ package com.dsi.ecommerce.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.swing.plaf.synth.SynthDesktopIconUI;
 
 @Controller
 public class CartController {
         @GetMapping("/cart/user/{id}")
         public String home (){
 
-                System.out.println("adlsfkjalsfjlasjdflajsflj");
-
                 return "cart/index";
         }
-
 }
