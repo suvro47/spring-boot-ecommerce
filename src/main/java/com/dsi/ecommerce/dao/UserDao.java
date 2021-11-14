e552f9141ac86fba37c0868a5accfcdb7296d9f1
Rashid <rashidul.hasan@dsinnovators.com>
2021-11-14 16:15:43 +0600

    Fix incorrect username in login

--- src/main/java/com/dsi/ecommerce/dao/UserDao.java
+++ src/main/java/com/dsi/ecommerce/dao/UserDao.java
@@ -5,9 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserDao extends JpaRepository<User, Long> {
 
     @Query("select u from users u where u.username = ?1")
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
 }
