8cd35171752c738217e5824b1266ba99a5630746
NahidReza08 <shovo1999@gmail.com>
2021-11-15 12:38:40 +0600

    Add Products Page

--- src/main/java/com/dsi/ecommerce/controller/ProductController.java
+++ src/main/java/com/dsi/ecommerce/controller/ProductController.java
@@ -1,11 +1,13 @@
 package com.dsi.ecommerce.controller;
 
+import com.dsi.ecommerce.dto.ProductDTO;
 import com.dsi.ecommerce.model.Product;
 import com.dsi.ecommerce.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
@@ -15,10 +17,17 @@ public class ProductController {
     ProductService productService;
 
     @RequestMapping("/products")
-    public String getHomeMovies(Model model) {
+    public String getAllProducts(Model model) {
         List<Product> products = productService.getProducts();
         model.addAttribute("products", products);
         return "product/products";
+    }
+
+    @RequestMapping(value = "/add-product", method = RequestMethod.GET)
+    public String getProductAddForm(Model model) {
+        ProductDTO productDTO = new ProductDTO();
+        model.addAttribute("product", productDTO);
+        return "product/products_add_form";
     }
 
 }
--- src/main/java/com/dsi/ecommerce/controller/UserController.java
+++ src/main/java/com/dsi/ecommerce/controller/UserController.java
@@ -15,7 +15,6 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
-@RequestMapping("/users")
 @Controller
 public class UserController {
     @Autowired
